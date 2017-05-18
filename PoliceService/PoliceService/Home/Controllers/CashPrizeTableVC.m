f1f4c2a0d648c40ec0d87fea8009098b2563dc68
吕乐乐 <lvlele423@126.com>
2017-05-18 15:41:41 +0800

    根据原型重新设计了有奖举报兑奖界面

--- PoliceService/PoliceService/Home/Controllers/CashPrizeTableVC.m
+++ PoliceService/PoliceService/Home/Controllers/CashPrizeTableVC.m
@@ -7,14 +7,22 @@
 //
 
 #import "CashPrizeTableVC.h"
+#import "PopoverView.h"
 
-@interface CashPrizeTableVC ()
+@interface CashPrizeTableVC (){
+    NSArray *typeArray;//存放cell
+    NSArray *wxArray;
+    NSArray *phoneArray;
+    NSArray *bankArray;
+    NSArray *cashArray;
+}
 @property (weak, nonatomic) IBOutlet UITextField *wechatTF;
 @property (weak, nonatomic) IBOutlet UITextField *phoneTF;
 @property (weak, nonatomic) IBOutlet UITextField *bankNameTF;
 @property (weak, nonatomic) IBOutlet UITextField *userNameTF;
 @property (weak, nonatomic) IBOutlet UITextField *cardNumTF;
 @property (weak, nonatomic) IBOutlet UITextField *cashCodeTF;
+@property (weak, nonatomic) IBOutlet UILabel *acceptTypeLabel;
 
 @end
 
@@ -29,7 +37,17 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     [self setUpLeftNavbarItem];
+    [self setupAreaAndClassLabel];
     self.title = @"有奖举报兑奖";
+    
+    typeArray = @[_cashTypeCell];
+    wxArray   = @[_wxTitleCell,_wxAccountCell];
+    phoneArray= @[_phoneTitleCell,_phoneNumCell];
+    bankArray = @[_bankTitleCell,_bankNameCell,_userNameCell,_bankCardNumCell];
+    cashArray = @[_cashTitleCell,_cashPrizeCell,_cutLineCell];
+    
+    showType = CashPrizeTypeDefault;
+    
 }
 - (void)setUpLeftNavbarItem {
     //    typeof(self) __weak wself = self;
@@ -37,35 +55,125 @@
         
     }];
 }
-- (IBAction)submit:(id)sender {
+- (void)setupAreaAndClassLabel {
+    self.acceptTypeLabel.font = [UIFont fontWithName:@"iconfont" size:15.0];
+    self.acceptTypeLabel.text = @"请选择领奖方式 \U0000E601";
+
+}
+- (IBAction)submit:(UITapGestureRecognizer *)sender {
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)chooseType:(UITapGestureRecognizer *)sender {
+    NSMutableArray *popOverActions = [[NSMutableArray alloc] init];
+    NSArray *titleArr = @[@"微信红包", @"手机号充值", @"银行转账"];
+    
+    typeof(self) __weak wself = self;
+    [titleArr enumerateObjectsUsingBlock:^(NSString  *businessName, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        PopoverAction *action = [PopoverAction actionWithTitle:businessName handler:^(PopoverAction *action) {
+            NSLog(@"fontName = %@", businessName);
+            wself.acceptTypeLabel.text = [NSString stringWithFormat:@"%@ \U0000E601", businessName];
+            
+
+            switch (idx) {
+                case 0:
+                    showType = CashPrizeTypeWX;
+                    break;
+                case 1:
+                  showType = CashPrizeTypePhone;
+                    break;
+                case 2:
+                    showType = CashPrizeTypeBank;
+                    break;
+                default:
+                    break;
+            }
+            [self.tableView reloadData];
+            
+        }];
+        [popOverActions addObject:action];
+    }];
+    
+    
+    PopoverView *popoverView = [PopoverView popoverView];
+    popoverView.style = PopoverViewStyleDark;
+    popoverView.hideAfterTouchOutside = YES; // 点击外部时允许隐藏
+    [popoverView showToView:self.acceptTypeLabel withActions:popOverActions];
+}
 
 #pragma mark - Table view data source
-//
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//#warning Incomplete implementation, return the number of sections
-//    return 0;
-//}
-//
-//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//#warning Incomplete implementation, return the number of rows
-//    return 0;
-//}
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    if (showType == CashPrizeTypeDefault) {
+        return 2;
+    }
+    return 3;
 }
-*/
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return indexPath.section !=0 && indexPath.row == 0 ? 30:50;
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    if (section == 0) {
+        return 1;
+    }
+    switch (showType) {
+        case CashPrizeTypeDefault:
+        {
+            return cashArray.count;
+        }
+        case CashPrizeTypeWX:
+        {
+            return section == 1? wxArray.count :cashArray.count;
+        }
+            break;
+        case CashPrizeTypePhone:
+        {
+            return section == 1? phoneArray.count :cashArray.count;
+        }
+        case CashPrizeTypeBank:
+        {
+            return section == 1? bankArray.count :cashArray.count;
+        }
+    }
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == 0) {
+        return typeArray[indexPath.row];
+    }
+    switch (showType) {
+        case CashPrizeTypeDefault:
+        {
+            return cashArray[indexPath.row];
+        }
+        case CashPrizeTypeWX:
+        {
+            return indexPath.section == 1? wxArray[indexPath.row] :cashArray[indexPath.row];
+        }
+            break;
+        case CashPrizeTypePhone:
+        {
+            return indexPath.section == 1? phoneArray[indexPath.row] :cashArray[indexPath.row];
+        }
+        case CashPrizeTypeBank:
+        {
+            return indexPath.section == 1? bankArray[indexPath.row] :cashArray[indexPath.row];
+        }
+    }
+    
+    return nil;
+}
+
 
 /*
 // Override to support conditional editing of the table view.
